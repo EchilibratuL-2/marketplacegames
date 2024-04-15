a5fa84f243e8e366edd1efa8a2b3adab09da4652
Mircea72 <mircris36@yahoo.com>
2024-04-15 20:14:35 +0300

    Header Style

--- src/index.js
+++ src/index.js
@@ -157,5 +157,19 @@ function initEvents() {
   });
 }
 
+/*  Header circular-text style  */
+
+const str = "  Marketplace   *  *  *  *   Games  *  *  *  *  ";
+const text = document.getElementById("text-rotate");
+window.onload = function () {
+  for (let i = 0; i < str.length; i++) {
+    let span = document.createElement("span");
+    span.innerHTML = str[i];
+    text.appendChild(span);
+    console.log(str[i]);
+    span.style.transform = `rotate(${8 * i}deg)`;
+  }
+};
+
 initEvents();
 loadTeams();
